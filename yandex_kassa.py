8496277c4503899e342fc3aca7105927355e1a9d
Konstantin Kruglov <kruglovk@gmail.com>
2017-02-12 12:21:22 +0300

    refactoring

--- yandex_kassa.py
+++ yandex_kassa.py
@@ -1,89 +1,37 @@
 # coding: utf-8
 
 import logging
-import datetime
 from hashlib import md5
+from datetime import datetime
 from urllib.parse import parse_qs
-
-from django.conf import settings
-from django.http import HttpResponse
-
-logger = logging.getLogger('logs')
+import xml.etree.ElementTree as ET
 
 
-class YandexKassa:
-    fields = []
+def get_logger(log_name='yandex_kassa'):
+    """Получение логгера."""
+    return logging.getLogger(log_name)
 
-    def __init__(self, body):
-        self.raw_body = body
-        self.cleaning_data = {}
-        self.shop_id = getattr(settings, 'YANDEX_KASSA_SHOP_ID', None)
-        self.sc_id = getattr(settings, 'YANDEX_KASSA_SHOP_SC_ID', None)
-        self.shop_password = getattr(
-            settings, 'YANDEX_KASSA_SHOP_PASSWORD', None)
 
-        logger.info('receive data (RAW): %s', self.raw_body)
+class BaseYandexKassa:
+    """Базовый класс."""
 
-        raw = parse_qs(self.raw_body)
-
-        for key, value in raw.items():
-            # delete: \\\n
-            self.cleaning_data[key] = value[0].strip().strip('\\').strip()
-
-    def _gen_md5(self, order_sum):
-        """
-        Генерация MD5 суммы пришедших от Yandex параметров, как указано в
-        документации:
-
-            action;orderSumAmount;orderSumCurrencyPaycash;orderSumBankPaycash;shopId;invoiceId;customerNumber;shopPassword
-
-        :return: bool
-        """
-
-        md5_fields = [
-            self.cleaning_data['action'],
-            order_sum,
-            self.cleaning_data['orderSumCurrencyPaycash'],
-            self.cleaning_data['orderSumBankPaycash'],
-            self.shop_id,
-            self.cleaning_data['invoiceId'],
-            self.cleaning_data['customerNumber'],
-            self.shop_password
-        ]
-
-        md5_str = ';'.join(map(str, md5_fields))
-        md5_sum = md5(md5_str.encode()).hexdigest().upper()
-
-        logger.info('my MD5 sum: %s (%s)', md5_sum, md5_str)
-        logger.info('receive MD5: %s', self.cleaning_data['md5'])
-
-        if md5_sum == self.cleaning_data['md5']:
-            logger.info('the MD5 order and the MD5 yandex is equal')
-            return True
-        else:
-            logger.warning('the MD5 order and the MD5 yandex is not equal')
-            return False
-
-    def check_md5(self, order_sum):
-        return self._gen_md5(order_sum)
-
-    def check_action(self):
-        raise NotImplementedError()
-
-    def check_shop(self):
-        """
-        Проверка тому ли магазину пришел запрос.
-
-        :return: bool
-        """
-        if self.shop_id == self.cleaning_data['shopId']:
-            if self.sc_id == self.cleaning_data['scid']:
-                return True
-
-        logger.info('unknown shop IDs')
-        return False
+    def __init__(self, shop_id, sc_id, shop_password, raw_data, log_name=None):
+        self.logger = get_logger(log_name) if log_name else get_logger()
+        self.shop_id = shop_id
+        self.sc_id = sc_id
+        self.shop_password = shop_password
+        self.raw_data = parse_qs(raw_data)
+        self.cleaning_data = self.full_clean()
+        self.action = self.cleaning_data['action']
 
     def full_clean(self):
+        """Проводим данные к читаемому виду."""
+        cleaning_data = {}
+
+        for key, value in self.raw_data.items():
+            # delete: \\\n
+            cleaning_data[key] = value[0].strip().strip('\\').strip()
+
         fields = [
             # ['requestDatetime', ''],
             ['action', str],
@@ -107,88 +55,121 @@ class YandexKassa:
             ['paymentType', str],
         ]
 
-        for item in fields:
+        for field, type_ in fields:
             try:
-                self.cleaning_data[item[0]] = item[1](
-                    self.cleaning_data[item[0]])
+                cleaning_data[field] = type_(cleaning_data[field])
             except KeyError:
                 continue
 
-        logger.info('clean data: %s', self.cleaning_data)
+        self.logger.info('clean data: %s', self.cleaning_data)
 
-    def get_xml(self, code, message=None):
-        if code == 0:
-            xml = '<?xml version="1.0" encoding="UTF-8"?><checkOrderResponse' \
-                ' performedDatetime="{date}" code="{return_code}"' \
-                ' invoiceId="{invoice_id}" shopId="{shop_id}" />'.format(
-                    date=datetime.datetime.now().isoformat(),
-                    return_code=code,
-                    invoice_id=self.cleaning_data['invoiceId'],
-                    shop_id=self.cleaning_data['shopId'])
-        elif code == 1:
-            xml = '<?xml version="1.0" encoding="UTF-8"?><checkOrderResponse' \
-                  ' performedDatetime="{date}" code="{return_code}"' \
-                  ' message="{message}" />'.format(
-                    date=datetime.datetime.now().isoformat(),
-                    return_code=code,
-                    message=message or 'MD5 not matched')
-        elif code == 100:
-            fields = {
-                'date': datetime.datetime.now().isoformat(),
-                'return_code': code,
-                'message': 'unknown error'
-            }
-            xml = '<?xml version="1.0" encoding="UTF-8"?><checkOrderResponse' \
-                  ' performedDatetime="{date}" code="{return_code}"' \
-                  ' message="{message}" />'.format(**fields)
-        elif code == 200:
-            xml = '<?xml version="1.0" encoding="UTF-8"?><checkOrderResponse' \
-                  ' performedDatetime="{date}" code="{return_code}"' \
-                  ' message="{message}" />'.format(
-                    date=datetime.datetime.now().isoformat(),
-                    return_code=code,
-                    message='unknown error')
+        return cleaning_data
+
+    def check_shop(self):
+        """
+        Проверка тому ли магазину пришел запрос.
+
+        :return: bool
+        """
+        if self.shop_id == self.cleaning_data['shopId']:
+            if self.sc_id == self.cleaning_data['scid']:
+                return True
+
+        self.logger.info('unknown shop IDs')
+        return False
+
+    def check_md5_sum(self, order_sum):
+        """
+        Генерация MD5 суммы пришедших от Yandex параметров, как указано в
+        документации:
+
+            action;orderSumAmount;orderSumCurrencyPaycash;orderSumBankPaycash;
+            shopId;invoiceId;customerNumber;shopPassword
+
+        :return: bool
+        """
+
+        md5_fields = [
+            self.cleaning_data['action'],
+            order_sum,
+            self.cleaning_data['orderSumCurrencyPaycash'],
+            self.cleaning_data['orderSumBankPaycash'],
+            self.shop_id,
+            self.cleaning_data['invoiceId'],
+            self.cleaning_data['customerNumber'],
+            self.shop_password
+        ]
+
+        md5_str = ';'.join(map(str, md5_fields))
+        md5_sum = md5(md5_str.encode()).hexdigest().upper()
+
+        if md5_sum == self.cleaning_data['md5']:
+            self.logger.info('the MD5 order and the MD5 yandex is equal')
+            return True
         else:
-            xml = '<?xml version="1.0" encoding="UTF-8"?><checkOrderResponse' \
-                  ' performedDatetime="{date}" code="{return_code}"' \
-                  ' message="{message}" />'.format(
-                    date=datetime.datetime.now().isoformat(),
-                    return_code=code,
-                    message='unknown error')
-
-        return xml
+            self.logger.warning(
+                'the MD5 order and the MD5 yandex is not equal'
+            )
+            return False
 
     def get_customer_number(self):
         """ Идентификатор плательщика на стороне магазина """
-        return self.cleaning_data['customerNumber']
-
-    def response(self, code):
-        return HttpResponse(self.get_xml(code), content_type='application/xml')
-
-
-class CheckOrder(YandexKassa):
-    """
-    https://tech.yandex.ru/money/doc/payment-solution/payment-notifications/payment-notifications-check-docpage/
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(CheckOrder, self).__init__(*args, **kwargs)
-
-        self.full_clean()
-
-    def check_action(self):
-        return self.cleaning_data['action'] == 'checkOrder'
+        return self.cleaning_data.get('customerNumber', None)
 
     def get_order_sum(self):
         """ Сумма заказа """
-        return self.cleaning_data['orderSumAmount']
+        return self.cleaning_data.get('orderSumAmount', None)
 
     def get_shop_sum(self):
         """ Сумма выплачиваемая магазину с вычетом комиссии Yandex """
-        return self.cleaning_data['shopSumAmount']
+        return self.cleaning_data.get('shopSumAmount', None)
+
+    def check_action(self):
+        """Метод проверки события, нужна реализация в дочерних классах."""
+        raise NotImplementedError()
+
+    def _get_responce(self, **kwargs):
+        attrs = {
+            'performedDatetime': datetime.now().isoformat(),
+            'code': kwargs.get('code'),
+            'invoiceId': self.cleaning_data.get('invoiceId'),
+            'shopId': self.shop_id
+        }
+
+        if kwargs.get('message'):
+            attrs.update({'message': kwargs.get('message')})
+
+        if kwargs.get('techMessage'):
+            attrs.update({'techMessage': kwargs.get('techMessage')})
+
+        root = {
+            'checkOrder': 'checkOrderResponse',
+            'paymentAviso': 'paymentAvisoResponse'
+        }
+
+        res_xml = ET.Element(root[self.action], **attrs)
+
+        return ET.tostring(res_xml)
+
+    def get_responce(self, code, message=None, tech_message=None):
+        """Получение ответа для яндекса."""
+        return self._get_responce(
+            **{'code': code, 'message': message, 'techMessage': tech_message})
 
 
-class PaymentAviso(YandexKassa):
+class CheckOrder(BaseYandexKassa):
+    """
+    https://tech.yandex.ru/money/doc/payment-solution/payment-notifications/payment-notifications-check-docpage/
+    """
+    def __init__(self, *args, **kwargs):
+        super(CheckOrder, self).__init__(*args, **kwargs)
+
+    def check_action(self):
+        """Проверяем событие."""
+        return self.action == 'checkOrder'
+
+
+class PaymentAviso(BaseYandexKassa):
     """
     https://tech.yandex.ru/money/doc/payment-solution/payment-notifications/payment-notifications-aviso-docpage/
     """
@@ -196,13 +177,6 @@ class PaymentAviso(YandexKassa):
     def __init__(self, *args, **kwargs):
         super(PaymentAviso, self).__init__(*args, **kwargs)
 
-        self.full_clean()
-
     def check_action(self):
-        return self.cleaning_data['action'] == 'paymentAviso'
-
-
-class Client(YandexKassa):
-    def __init__(self, *args, **kwargs):
-        super(Client, self).__init__(*args, **kwargs)
-        self.url = 'https://penelope-demo.yamoney.ru:8083'
+        """Проверяем событие."""
+        return self.action == 'paymentAviso'
