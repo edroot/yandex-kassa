a5505506612cb529eb975a1abe5bcfc7f3524e13
Konstantin Kruglov <kruglovk@gmail.com>
2017-02-13 10:59:41 +0300

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 # coding: utf-8
 
+import os
 from setuptools import setup, find_packages
+
+
+def get_version():
+    bd = os.path.dirname(__file__)
+
+    with open(os.path.join(bd, 'yandex_kassa/version.py')) as f:
+        version = dict()
+        exec(f.read(), version)
+
+        return version['VERSION']
+
 
 setup(
     name='yandex-kassa',
-    version='0.0.2',
+    version=get_version(),
     author='Konstantin Kruglov',
     author_email='kruglovk@gmail.com',
     description='empty',
